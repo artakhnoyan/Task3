44513033b0a97305ddd7c6f51d82938776ce7306
Arthur <aakhnoyan@gmail.com>
2019-02-18 23:58:04 +0400

    executor canceled if started

--- app/src/main/java/com/art/task3/domain/UseCase.kt
+++ app/src/main/java/com/art/task3/domain/UseCase.kt
@@ -1,13 +1,16 @@
 package com.art.task3.domain
 
 import java.util.concurrent.Executors
+import java.util.concurrent.Future
 
 private const val THREAD_NUMBER: Int = 1
 
 open class UseCase {
     private val executorService = Executors.newFixedThreadPool(THREAD_NUMBER)
+    private var future: Future<*>? = null
 
     protected fun execute(block: () -> Unit) {
-        executorService.execute(block)
+        future?.cancel(true)
+        future = executorService.submit(block)
     }
 }
